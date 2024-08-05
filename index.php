eac49aabad839f2773f50ef3238932cf9bfdf839
Samukelo Worthwhile Makofane <samukelomakofane@gmail.com>
2024-08-05 21:53:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//Include Header
+include('theme/header.php');
+
+?>
+
+<section class="index-section1">
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</section>
+
+<?php
+//Include Footer
+include('theme/footer.php');
+
+?>
---
+++ theme/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>SA Exotics Auto & Real Estate</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
+
+        <!-- Google Web Fonts -->
+        <link rel="icon" type="image/x-icon" href="assets/imgs/favicon.ico"/>
+
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wdth,wght@0,75..100,300..800;1,75..100,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet"> 
+
+        <!-- Icon Font Stylesheet -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link href="assets/css/style.css" rel="stylesheet">
+    </head>
+    
+    <body>
+    <!-- Navigation Bar -->
+
+
+<nav class="navbar navbar-dark navbar-expand-sm bg-dark fixed-top">
+        <div class="container">
+        <a href="/" class="navbar-brand">
+        <i class="fas fa-blog"></i> &nbsp;
+        Blog Template
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a href="" class="nav-link active">
+                    Home
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link active">
+                    Blog
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link active">
+                    About
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link active">
+                    Contact
+                </a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
+  
